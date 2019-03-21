67843ee7d6e569628f9057b1d080be9c3143f617
Andreas Van Assche <andreasava@hotmail.com>
2019-03-21 12:18:39 +0100

    changes

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -18,8 +18,24 @@ function activeNavigation() {
       }
     });
   });
-}
+};
+
+  // Header scroll class
+function headerScroll() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+};
 
 
 
 export { activeNavigation };
+export { headerScroll };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,27 +3,48 @@ import "jquery";
 
 // import local javascript
 import { activeNavigation } from '../components/navbar'
+import { headerScroll } from '../components/navbar'
 import { bannerVisuals } from '../components/banner';
 
+  // Smooth scroll for the menu and links with .scrollto classes
+  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
 
+        if ($('#header').length) {
+          top_space = $('#header').outerHeight();
 
-  // Header scroll class
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('#header').addClass('header-scrolled');
-    } else {
-      $('#header').removeClass('header-scrolled');
+          if (! $('#header').hasClass('header-scrolled')) {
+            top_space = top_space - 20;
+          }
+        }
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - top_space
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu').length) {
+          $('.nav-menu .menu-active').removeClass('menu-active');
+          $(this).closest('li').addClass('menu-active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+        return false;
+      }
     }
   });
-
-  if ($(window).scrollTop() > 100) {
-    $('#header').addClass('header-scrolled');
-  }
 
 
 
 
 activeNavigation();
+headerScroll();
 bannerVisuals();
 
 
