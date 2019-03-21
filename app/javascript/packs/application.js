d77e256584946d76445bde62f0280b74ad41d343
Andreas Van Assche <andreasava@hotmail.com>
2019-03-21 11:38:48 +0100

    changes to header

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,28 @@ import "bootstrap";
 import "jquery";
 
 // import local javascript
+import { activeNavigation } from '../components/navbar'
 import { bannerVisuals } from '../components/banner';
+
+
+
+  // Header scroll class
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+
+
+
+activeNavigation();
 bannerVisuals();
+
+
