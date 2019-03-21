ce6e8621d972d76a0cba8714752344c09aa6c9d0
Andreas Van Assche <andreasava@hotmail.com>
2019-03-21 15:21:49 +0100

    smooth scroll

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,47 +6,12 @@ import "webpack-jquery-ui/css";
 // import local javascript
 import { activeNavigation } from '../components/navbar'
 import { headerScroll } from '../components/navbar'
+import { smoothScroll } from '../components/navbar'
 import { bannerVisuals } from '../components/banner';
-
-  // Smooth scroll for the menu and links with .scrollto classes
-  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      if (target.length) {
-        var top_space = 0;
-
-        if ($('#header').length) {
-          top_space = $('#header').outerHeight();
-
-          if (! $('#header').hasClass('header-scrolled')) {
-            top_space = top_space - 20;
-          }
-        }
-
-        $('html, body').animate({
-          scrollTop: target.offset().top - top_space
-        }, 1500, 'easeInOutExpo');
-
-        if ($(this).parents('.nav-menu').length) {
-          $('.nav-menu .menu-active').removeClass('menu-active');
-          $(this).closest('li').addClass('menu-active');
-        }
-
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-          $('#mobile-body-overly').fadeOut();
-        }
-        return false;
-      }
-    }
-  });
-
-
-
 
 activeNavigation();
 headerScroll();
+smoothScroll();
 bannerVisuals();
 
 
